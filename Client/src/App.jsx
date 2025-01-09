048861f1658b20c1b7f88f3d7d64beacfcd9b47d
ayrelmay <152473022+ayrelmay@users.noreply.github.com>
2025-01-09 15:32:02 +0800

    admin route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Applayout from "./ui/applayout";
 import Dashbaord from "./pages/Admin/Dashboard";
 import Payment from "./pages/Admin/Payment";
@@ -10,79 +9,26 @@ import Appointment from "./pages/Admin/Appointment";
 import Inventory from "./pages/Admin/Inventory";
 import InvoiceHistory from "./pages/Admin/InvoiceHistory";
 import Accounts from "./pages/Admin/Accounts";
-import Login from "./pages/Client/Login";
-import CahsierAppLayout from "./ui/CahsierApplayout";
-import TechAppLayout from "./ui/TechApplayout";
-import { AuthProvider } from "./context/AuthContext";
-import ClientAppLayout from "./ClientSide/clientApplayout";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Client Routes */}
-          <Route path="/*" element={<ClientAppLayout />} />
-          <Route path="/login" element={<Login />} />
-
-          {/* Admin Routes */}
-          <Route
-            path="/admin/*"
-            element={
-              <ProtectedRoute allowedRoles={["admin"]}>
-                <Applayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="dashboard" element={<Dashbaord />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="payment/edit/:id" element={<PaymentEdit />} />
-            <Route path="services" element={<Services />} />
-            <Route path="appointment" element={<Appointment />} />
-            <Route path="inventory" element={<Inventory />} />
-            <Route path="invoicehistory" element={<InvoiceHistory />} />
-            <Route path="accounts" element={<Accounts />} />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Applayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashbaord />} />
+          <Route path="payment">
+            <Route index element={<Payment />} />
+            <Route path="edit/:id" element={<PaymentEdit />} />
           </Route>
-
-          {/* Cashier Routes */}
-          <Route
-            path="/cashier/*"
-            element={
-              <ProtectedRoute allowedRoles={["cashier"]}>
-                <CahsierAppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="dashboard" element={<Dashbaord />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="payment/edit/:id" element={<PaymentEdit />} />
-            <Route path="services" element={<Services />} />
-            <Route path="appointment" element={<Appointment />} />
-            <Route path="inventory" element={<Inventory />} />
-            <Route path="invoicehistory" element={<InvoiceHistory />} />
-            <Route path="accounts" element={<Accounts />} />
-          </Route>
-
-          {/* Technician Routes */}
-          <Route
-            path="/technician/*"
-            element={
-              <ProtectedRoute allowedRoles={["technician"]}>
-                <TechAppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="services" element={<Services />} />
-            <Route path="appointment" element={<Appointment />} />
-            <Route path="inventory" element={<Inventory />} />
-            <Route path="invoicehistory" element={<InvoiceHistory />} />
-            <Route path="accounts" element={<Accounts />} />
-          </Route>
-
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+          <Route path="services" element={<Services />} />
+          <Route path="appointment" element={<Appointment />} />
+          <Route path="inventory" element={<Inventory />} />
+          <Route path="invoicehistory" element={<InvoiceHistory />} />
+          <Route path="accounts" element={<Accounts />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
